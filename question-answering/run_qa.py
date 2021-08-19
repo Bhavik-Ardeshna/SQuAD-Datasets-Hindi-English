c93172b2a8b229cf1db83d9d22206e46941bbc8d
Bhavik-Ardeshna <ardeshnabhavik@gmail.com>
2021-08-19 12:00:12 +0530

    final change

--- question-answering/run_qa.py
+++ question-answering/run_qa.py
@@ -264,7 +264,7 @@ def main():
         # raw_datasets = load_dataset(
         #     data_args.dataset_name, data_args.dataset_config_name, cache_dir=model_args.cache_dir
         # )
-        dataset = load_dataset(<path to changed squad.py dataloader>, data_files={'train': <train-path>, 'validation': <validation-path>})
+        raw_datasets = load_dataset("/content/SQuAD-Datasets-Hindi-English/question-answering/squad.py", data_files={'train': '/content/SQuAD-Datasets-Hindi-English/train-v1.1.json', 'validation': '/content/SQuAD-Datasets-Hindi-English/dev-v1.1.json'})
 
     else:
         data_files = {}
@@ -278,8 +278,10 @@ def main():
         if data_args.test_file is not None:
             data_files["test"] = data_args.test_file
             extension = data_args.test_file.split(".")[-1]
-        raw_datasets = load_dataset(extension, data_files=data_files, field="data", cache_dir=model_args.cache_dir)
-    # See more about loading any type of standard or custom dataset (from files, python dict, pandas DataFrame, etc) at
+        # raw_datasets = load_dataset(extension, data_files=data_files, field="data", cache_dir=model_args.cache_dir)
+        raw_datasets = load_dataset("squad.py", data_files={'train': data_files["train"], 'dev': data_files["validation"]})
+
+    #  See more about loading any type of standard or custom dataset (from files, python dict, pandas DataFrame, etc) at
     # https://huggingface.co/docs/datasets/loading_datasets.html.
 
     # Load pretrained model and tokenizer
